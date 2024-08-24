10c01b0e738aae25254b29a9734288f76b8c5f1d
Thais Marques Mota <thaismferreira42@gmail.com>
2024-08-24 01:00:28 +0000

    aula 23/08

--- app/Componentes/Adaptadores/ListaProdutos/index.tsx
+++ app/Componentes/Adaptadores/ListaProdutos/index.tsx
@@ -1,8 +1,13 @@
 import { View } from "react-native";
 import  ItemProduto from "../ItemProdutos"
+import Style from "@/app/Estilos/Default";
+import { ScrollView } from "react-native-gesture-handler";
 
 function ListaProdutos({produtos}:{produtos:{id:number, nome:string, preco:number} []}){
-    return(<View>
+    return(
+    
+    <ScrollView>
+        <View style={Style.container}>
         {produtos.map((p)=>
         <ItemProduto produto={p} key={p.id}>
 
@@ -10,6 +15,7 @@ function ListaProdutos({produtos}:{produtos:{id:number, nome:string, preco:numbe
 
 
     </View>
+    </ScrollView>
     )
 }
 
